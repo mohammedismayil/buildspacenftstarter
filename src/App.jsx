725a9c9bfe8d5056ca8e36ecae1e043dca910262
Ismayil <30821770+mohammedismayil@users.noreply.github.com>
2022-05-24 03:34:41 +0000

    removed template contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import './styles/App.css';
-import twitterLogo from './assets/twitter-logo.svg';
-
-// Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
-const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
   // Render Methods
@@ -27,13 +20,13 @@ const App = () => {
           {renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          
           <a
             className="footer-text"
-            href={TWITTER_LINK}
+            
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`Made with  ❤  by ismayil`}</a>
         </div>
       </div>
     </div>
