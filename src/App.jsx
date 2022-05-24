54ac23b9bc39c348a971ae379d228d606b8789b2
Ismayil <30821770+mohammedismayil@users.noreply.github.com>
2022-05-24 04:15:55 +0000

    imported smartcontract and interacted with the same

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,101 @@
-import React from 'react';
-import './styles/App.css';
+import { ethers } from "ethers";
 
+import React, { useEffect, useState } from "react";
+import './styles/App.css';
+import TycoonFixedWordsNFT from './TycoonWithWords.json'
+const provider = new ethers.providers.Web3Provider(ethereum);
+const signer = provider.getSigner();
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
+  /*
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      /*
+      * Fancy method to request access to account.
+      */
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      /*
+      * Boom! This should print out public address once we authorize Metamask.
+      */
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]); 
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
 
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+  /*
+  * Added a conditional render! We don't want to show Connect to Wallet if we're already connected :).
+  */
+  const askContractToMintNft = async () => {
+  const CONTRACT_ADDRESS = "0x039cc71777C006a01D7fa7054712dC023cE8ee90";
+
+  try {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, TycoonFixedWordsNFT.abi, signer);
+
+      console.log("Going to pop wallet now to pay gas...")
+      let nftTxn = await connectedContract.makeAnEpicNFT();
+
+      console.log("Mining...please wait.")
+      await nftTxn.wait();
+      
+      console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+
+    } else {
+      console.log("Ethereum object doesn't exist!");
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
   return (
     <div className="App">
       <div className="container">
@@ -17,16 +104,22 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? (
+            renderNotConnectedContainer()
+          ) : (
+            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+        Mint NFT
+      </button>
+          )}
         </div>
         <div className="footer-container">
           
-          <a
+           <a
             className="footer-text"
             
             target="_blank"
             rel="noreferrer"
-          >{`Made with  ❤  by ismayil`}</a>
+          >{`Made with  ❤  by Ismayil`}</a>
         </div>
       </div>
     </div>
